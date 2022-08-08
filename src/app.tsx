9cdc38244eddf6f0ab088c36b18e767d21fd5700
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-08-08 16:22:58 +0400

    add eslint checks

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-export function App(props: { name: string }): any {
+export function App(props: { name: string }): React.ReactElement {
   return <h1>Hello, {props.name}</h1>;
 }
