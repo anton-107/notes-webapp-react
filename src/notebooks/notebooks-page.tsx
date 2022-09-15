3ead58341b97b727d59faf08e526bf5b649aca60
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-09-15 19:21:44 +0200

    show a loading message on the list of notebooks

--- src/notebooks/notebooks-page.tsx
+++ src/notebooks/notebooks-page.tsx
@@ -5,11 +5,14 @@ import { Notebook, NotebooksService } from "./notebooks-service";
 import "./notebook-page.css";
 
 export function NotebooksPage(): React.ReactElement {
+  const [isLoading, setLoading] = useState(false);
   const [notebooks, setNotebooks] = useState([]);
 
   const loadNotebooks = async () => {
+    setLoading(true);
     const notebooksService = new NotebooksService();
     const notebooks = await notebooksService.listAll();
+    setLoading(false);
     setNotebooks(notebooks);
   };
 
@@ -19,6 +22,7 @@ export function NotebooksPage(): React.ReactElement {
   return (
     <div className="content-block">
       <h1 data-testid="notebooks-page-header">Notebooks</h1>
+      {isLoading && <div>Loading...</div>}
       {notebooks.map((n: Notebook) => {
         return (
           <div data-testid={`notebook-${n.id}`} key={`notebook-${n.id}`}>
