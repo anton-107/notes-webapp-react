605ffb3d69dab4d08692ba8d9a4abd7c2de5af7b
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-10-20 18:01:01 +0200

    show list of notes in the table view

--- src/notebooks/notebook-views/notebook-table.component.tsx
+++ src/notebooks/notebook-views/notebook-table.component.tsx
@@ -1,9 +1,39 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Note, NotesService } from "../../notes/notes-service";
 
 export function NotebookTableComponent(): React.ReactElement {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const { notebookID } = useParams();
+
+  const loadNotes = async () => {
+    const notesService = new NotesService();
+    const notes = await notesService.listAllForNotebook(notebookID);
+    setNotes(notes);
+  };
+  useEffect(() => {
+    loadNotes();
+  }, [location]);
+
   return (
     <div className="content-block" data-testid="notebook-table-view">
-      Table representation of notebook's notes
+      <table className="data-table">
+        <thead>
+          <tr>
+            <th>Note</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notes.map((n) => (
+            <tr data-testid={`note-row-${n.id}`}>
+              <td>{n.content}</td>
+              <td>{n.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- test/notebooks/notebook-views/notebook-table.component.test.tsx
+++ test/notebooks/notebook-views/notebook-table.component.test.tsx
@@ -2,12 +2,54 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom/extend-expect";
+import fetchMock from "jest-fetch-mock";
 import { render, waitFor, screen } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { NotebookTableComponent } from "../../../src/notebooks/notebook-views/notebook-table.component";
 
 describe("Notebook table component", () => {
+  beforeAll(() => {
+    fetchMock.mockResponse(async () => {
+      return JSON.stringify({
+        notes: [
+          {
+            content: "Note 1",
+            id: "note-1",
+            extensionProperties: { manualOrder: 100 },
+          },
+          {
+            content: "Note 2",
+            id: "note-2",
+            extensionProperties: { manualOrder: 200 },
+          },
+          {
+            content: "Note 3",
+            id: "note-3",
+            extensionProperties: { manualOrder: 300 },
+          },
+          {
+            content: "Note 4",
+            id: "note-4",
+          },
+          {
+            content: "Note 5",
+            id: "note-5",
+          },
+          {
+            content: "To do",
+            id: "todo-section",
+            type: { type: "notes-container" },
+          },
+          {
+            content: "To do item one",
+            id: "todo-1",
+            extensionProperties: { section: "todo-section" },
+          },
+        ],
+      });
+    });
+  });
   it("should render", async () => {
     const component = render(
       <BrowserRouter>
@@ -15,6 +57,7 @@ describe("Notebook table component", () => {
       </BrowserRouter>
     );
     await waitFor(() => screen.getByTestId("notebook-table-view"));
+    await waitFor(() => screen.getByTestId("note-row-note-1"));
     component.unmount();
   });
 });
