b167f1caa37ccfefc3ee14149c1d5ea4d7be0d5a
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-12-11 16:44:40 +0100

    set code quality rules: max-depth, max-nested-callbacks, max-lines-per-function, max-statements, max-params

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,5 +6,22 @@ module.exports = {
   rules: {
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
+    "no-duplicate-imports": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "max-depth": ["error", 3],
+    "max-nested-callbacks": ["error", 3],
+    "max-lines-per-function": ["error", 275],
+    "max-statements": ["error", 25],
+    "max-params": ["error", 3]
   },
+  overrides: [
+    {
+      files: ["**/*.test.ts", "**/*.dev.ts"],
+      rules: {
+        "max-lines-per-function": "off",
+        "max-statements": "off",
+        "max-nested-callbacks": "off",
+      },
+    },
+  ],
 };
