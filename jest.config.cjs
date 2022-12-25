0169b92d4194d33a42b35273b490e1507091ab70
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-12-25 21:20:31 +0100

    skip coverage for environment file

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       statements: 100,
     },
   },
-  collectCoverageFrom: ["src/**/*"],
+  collectCoverageFrom: ["src/**/{!(environment),}"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/file-mock.js",
