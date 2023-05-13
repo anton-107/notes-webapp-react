a1e866abd556a90f791bae21795031deaef86643
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-05-13 14:42:10 +0200

    expand filetree test scenario

--- test/notes/notes-filetree-service.test.ts
+++ test/notes/notes-filetree-service.test.ts
@@ -2,11 +2,13 @@ import { groupNotesAsFileTree } from "../../src/notes/notes-filetree-service";
 
 describe("groupNotesAsFileTree", () => {
   const notes = [
+    { content: "/file0" },
     { content: "/folder1/file1" },
     { content: "/folder1/file2" },
     { content: "/folder2/file1" },
     { content: "/folder3/file1" },
     { content: "/folder3/file2" },
+    { content: "/file3" },
   ];
 
   it("should return an empty array when notes are empty", () => {
@@ -15,13 +17,17 @@ describe("groupNotesAsFileTree", () => {
 
   it("should group notes by folder when current folder is root", () => {
     const result = groupNotesAsFileTree(notes, "/");
-    expect(result).toHaveLength(3);
+    expect(result).toHaveLength(5);
     expect(result[0].name).toBe("folder1");
     expect(result[0].isFolder).toBe(true);
     expect(result[1].name).toBe("folder2");
     expect(result[1].isFolder).toBe(true);
     expect(result[2].name).toBe("folder3");
     expect(result[2].isFolder).toBe(true);
+    expect(result[3].name).toBe("file0");
+    expect(result[3].isFolder).toBe(false);
+    expect(result[4].name).toBe("file3");
+    expect(result[4].isFolder).toBe(false);
   });
 
   it("should group notes by folder and subfolder when current folder is 'folder1'", () => {
