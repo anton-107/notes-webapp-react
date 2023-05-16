f1057b02af8cdb0aa3fcb04d420486dc1b8a8743
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-05-16 10:50:30 +0200

    navigate down into folders in a file tree

--- src/notebooks/notebook-views/notebook-filetree.component.tsx
+++ src/notebooks/notebook-views/notebook-filetree.component.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 import {
   FileEntry,
@@ -9,26 +9,39 @@ import {
 import { NotesService } from "../../notes/notes-service";
 
 export function NotebookFileTreeComponent(): React.ReactElement {
+  const location = useLocation();
   const { notebookID } = useParams();
   const [files, setFiles] = useState<FileEntry[]>([]);
+  const [currentPath, setCurrentPath] = useState<string>("");
 
-  const loadNotes = async () => {
+  const loadNotes = async (currentPath: string) => {
     const notesService = new NotesService();
     const notes = await notesService.listAllForNotebook(notebookID);
-    const files = groupNotesAsFileTree(notes, "/");
+    const files = groupNotesAsFileTree(notes, currentPath);
     setFiles(files);
   };
 
   useEffect(() => {
-    loadNotes();
+    const currentPath =
+      new URLSearchParams(location.search).get("currentPath") || "";
+    setCurrentPath(currentPath);
+    loadNotes(currentPath || "/");
   }, [location]);
 
   return (
     <div className="content-block" data-testid="notebook-filetree-view">
+      <div>Current path: {currentPath}</div>
       {files.map((file) => (
         <div key={`file-${file.name}`} data-testid={`file-${file.name}`}>
           {file.isFolder && <span>[folder] </span>}
-          {file.name}
+          {file.isFolder && (
+            <Link
+              to={`/notebook/${notebookID}/file-tree?currentPath=${currentPath}/${file.name}`}
+            >
+              {file.name}
+            </Link>
+          )}
+          {!file.isFolder && <span>{file.name}</span>}
         </div>
       ))}
     </div>
